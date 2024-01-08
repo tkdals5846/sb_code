7c857caf5c802a47818d2b3e3130b298da2afad5
tkdals5846 <tkdals5846@naver.com>
2024-01-08 10:58:12 +0000

    buildspec commit

--- start.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-sudo chmod 755 /home/ec2-user/sb_code1/target/springbootApp.jar
-sudo nohup java -jar /home/ec2-user/sb_code1/targe/springbootApp.jar 1> /dev/null 2>&1 &
-echo $! > pid.file
